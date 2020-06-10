56568a7f0b9ef5cce71c4dcf8a8b8b941c730690
jkboyal <63132964+jkboyal@users.noreply.github.com>
2020-06-09 21:16:24 -0400

    changes made
    updated all files

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# Final-Project
+# Final-Project : My Travel Bucketlist
+
 I am making this project as a part of my course IMT 549. 
+The purpose of this website is to kind of having a list of all the destinations I want to travel to, once the lockdown ends and after I graduate. The list provides city name, country name and cost of one person in USD per week. 
---
+++ destination.js
@@ -0,0 +1,91 @@
+const DESTINATION = [
+    {
+        "city": "Paris",
+        "country": "France",
+        "season": "June to August",
+        "cost": 3500
+
+    },
+    {
+        "city": "Canggu, Bali",
+        "country": "Indonesia",
+        "season": "April to October",
+        "cost": 1750
+    },
+    {
+        "city": "Tromso",
+        "country": "Norway",
+        "season": "September to April",
+        "cost": 900
+    },  
+    {
+        "city": "Thera, Santorini",
+        "country": "Greece",
+        "season": "April to September",
+        "cost": 1300
+    },  
+    {
+        "city": "Banff, Alberta",
+        "country": "Canada",
+        "season": "June to August",
+        "cost": 700
+    },  
+    {
+        "city": "Zurich",
+        "country": "Switzerland",
+        "season": "June to August",
+        "cost": 1500
+    },  
+    {
+        "city": "Orlando",
+        "country": "USA",
+        "season": "March to May",
+        "cost": 1800
+    },  
+    {
+        "city": "Nashville",
+        "country": "USA",
+        "season": "April to October",
+        "cost": 1050
+    },  
+    {
+        "city": "London",
+        "country": "UK",
+        "season": "March to June",
+        "cost": 1500
+    },  
+    {
+        "city": "Maui, Hawaii",
+        "country": "USA",
+        "season": "April to September",
+        "cost": 1200
+
+    },
+    {
+        "city": "Florence",
+        "country": "Italy",
+        "season": "May to September",
+        "cost": 1100
+
+    },
+    {
+        "city": "Vancouver",
+        "country": "Canada",
+        "season": "May to September",
+        "cost": 1000
+
+    },
+    {
+        "city": "Singapore",
+        "country": "Singapore",
+        "season": "December to June",
+        "cost": 1500
+
+    },
+    {
+        "city": "Kyoto",
+        "country": "Japan",
+        "season": "March to April",
+        "cost": 1400
+    }
+];
